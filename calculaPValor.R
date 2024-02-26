5d1c124a634bc6c0a91fbcebf09261b9f55e9e3f
MarioPasc <mario.pg02@gmail.com>
2024-02-26 20:26:40 +0100

    Funcion terminada-p valores

--- calculaPValor.R
+++ calculaPValor.R
@@ -1,4 +1,4 @@
-
+# Mario Pascual Gonzalez
 
 # Variable categórica - categórica
 # Chi-cuadrado
@@ -24,7 +24,7 @@ variables.categoricas <- function(datos) {
   )
 }
 
-calculaPValor <- function(datos, objetivo, var1, var2) {
+calculaPValor <- function(datos, var1, var2) {
   # Calculamos las variables categóricas del dataset
   varCat <- variables.categoricas(datos)
   
@@ -36,7 +36,8 @@ calculaPValor <- function(datos, objetivo, var1, var2) {
   # Calculamos la tabla de contingencia utilzando xtabs y construyendo una 
   # fórmula dinámica con los argumentos de la función
   formula.text <- paste("~", var1, "+", var2, sep = " ")
-  tabla.contingencia <- xtabs(formula = as.formula(formula.text), data = datos)
+  tabla.contingencia <- xtabs(formula = as.formula(formula.text), 
+                              data = datos)
   
   if (var1.cat && var2.cat) { # Ambas son categóricas
     # Se ha añadido un trycatch ya que hay veces que da error por el tamaño del
@@ -64,20 +65,99 @@ calculaPValor <- function(datos, objetivo, var1, var2) {
     
   } else if (!var1.cat && !var2.cat) { # Ambas son numéricas
     
+    if (
+      shapiro.test(datos[[var1]])$p.value > 0.05 &&
+      shapiro.test(datos[[var2]])$p.value > 0.05
+    ) {
+      print("PEARSON")
+      return(cor.test(datos[[var1]], datos[[var2]], method = "pearson")$p.value)
+    } else {
+      print("SPEARMAN")
+      return(cor.test(datos[[var1]], datos[[var2]], method = "spearman")$p.value)
+    }
+    
   } else { # Una de las dos es numérica
     
     # Almacenamos la que es numérica y la que no
     if (var1.cat) {
-      numerica <- var2.cat
-      categorica <- var1.cat
+      numerica <- var2
+      categorica <- var1
     } else {
-      numerica <- var1.cat
-      categorica <- var2.cat
+      numerica <- var1
+      categorica <- var2
     }
     
+    formula.text <- paste(var1, "~", var2, sep = " ")
     
+    if (length(unique(datos[[categorica]])) > 2) {
+      
+      if (shapiro.test(datos[[numerica]])$p.value > 0.05) {
+        # Más de 2 tipos, distribución normal -> AOV
+        aov.test <- summary(aov(formula = as.formula(formula.text), 
+                                data = datos))
+        print("AOV")
+        return(aov.test[[1]][["Pr(>F)"]][1])
+        
+      } else {
+        # Más de dos datos, distribución diferente -> Kruskal
+        kruskal.test <- kruskal.test(formula = as.formula(formula.text), 
+                                    data = datos)
+        print("KURSKAL")
+        return(kruskal.test$p.value)
+      }
+    } else {
+      
+      if (shapiro.test(datos[[numerica]])$p.value > 0.05) {
+        # 2 tipos, distribucion normal -> t test
+        ttest.test <- t.test(formula = as.formula(formula.text), 
+                             data = datos)
+        print("TTEST")
+        return(ttest.test$p.value)
+      } else {
+        # 2 tipos, distribucion diferente -> Wilcoxon test
+        wilcoxon.test <- wilcox.test(formula = as.formula(formula.text),
+                                     data = datos)
+        print("WILCOXON")
+        return(wilcoxon.test$p.value)
+      }
+    }
   }
 }
+
+aplicaCalculaPValorATodosLosPares <- function(datos) {
+  # Obtén todos los nombres de las variables del dataset
+  nombres_variables <- names(datos)
+  
+  # Inicializa un vector para almacenar los resultados
+  resultados_p_valores <- numeric(0) # Vector vacío
+  variable_1 <- character(0) 
+  variable_2 <- character(0)
+  
+  # Itera sobre cada combinación única de pares de variables
+  for (i in 1:(length(nombres_variables) - 1)) {
+    for (j in (i + 1):length(nombres_variables)) {
+      # Nombres de las variables actuales
+      var1 <- nombres_variables[i]
+      var2 <- nombres_variables[j]
+      
+      # Llama a calculaPValor para el par de variables actual
+      resultado <- calculaPValor(datos, var1, var2)
+      
+      # Almacena el resultado p en el vector
+      resultados_p_valores <- c(resultados_p_valores, resultado)
+      
+      # Guarda el nombre del par de variables
+      variable_1 <- c(variable_1, paste(var1))
+      variable_2 <- c(variable_2, paste(var2))
+      
+    }
+  }
+  
+  resultados_df <- data.frame(VariableX = variable_1, VariableY = variable_2, 
+                              Valor = resultados_p_valores)
+  return(resultados_df)
+}
+
 
 
 
